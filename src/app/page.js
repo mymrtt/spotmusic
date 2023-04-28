97339b572e15b5abe4cf2437170f88e19998f948
mymrtt <mymrtt@gmail.com>
2023-04-28 09:47:08 -0300

    chore: turn recommended component to receive new props

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,46 @@
+"use client";
+
+import useSWR from "swr";
 import Header from "./components/header";
 import Playlists from "./components/playlists";
 import Recommended from "./components/recommended";
 import NavBar from "./components/navbar";
 import Player from "./components/player";
+import { fetcher } from "./utils/fetcher";
 
 const Home = () => {
+	const {
+		data: yourShowsData,
+		error: yourShowsError,
+		isLoading: yourShowsIsLoading,
+		isValidating: yourShowsIsValidating,
+	} = useSWR(() => "/api/your-shows", fetcher);
+
+	const {
+		data: jumpBackInData,
+		error: jumpBackInError,
+		isLoading: jumpBackInIsLoading,
+		isValidating: jumpBackInIsValidating,
+	} = useSWR(() => "/api/jump-back-in", fetcher);
+
 	return (
 		<main className="flex min-h-screen flex-col px-3 pb-48">
 			<Header />
 			<Playlists />
-			<Recommended title="Your Shows" />
-			<Recommended title="Jump back in" />
+			<Recommended
+				title="Your Shows"
+				data={yourShowsData}
+				isValidating={yourShowsIsValidating}
+				isLoading={yourShowsIsLoading}
+				error={yourShowsError}
+			/>
+			<Recommended
+				title="Jump back in"
+				data={jumpBackInData}
+				isValidating={jumpBackInIsValidating}
+				isLoading={jumpBackInIsLoading}
+				error={jumpBackInError}
+			/>
 			<Player />
 			<NavBar />
 		</main>
